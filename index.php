cfcf28752550bd2180b66e76314e2c7aa70f0baa
paschalk1 <writermsa@gmail.com>
2017-02-06 16:38:27 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
+    ob_start("ob_gzhandler");
+else
+    ob_start();
+
+include_once 'config.php';
+
+# Start
+# Get module!
+$module = filter_input(INPUT_GET, 'module');
+
+# Init controller object!
+$controller = null;
+
+ #logout utility
+if($module === 'logout'){
+    Utils::logout();
+}
+
+if(isset($_SESSION[T_SUPERVISOR])) {
+    #route into supervisor's module
+    switch ($module){
+        case NULL:
+        case 'supervisor':
+           $controller=new SupervisorController();
+           break;
+        default :
+            $controller=new ErrorController();
+            break;
+    }
+}elseif(isset($_SESSION[T_USER])) {
+    # Route into user's module
+    switch ($module) {
+        case NULL:
+        case 'user':
+            $controller = new UserController();
+            break;
+        default :
+            $controller = new ErrorController();
+        break;
+    }
+}else{
+    #remain here on the auth page
+     $controller = new AuthController();
+}
+
+$controller -> __run();
